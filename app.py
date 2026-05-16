c6dac2c55123c107aed298f3f2fd5b6d48beaa55
Pratik Punj <pratikpunj3@gmail.com>
2026-05-17 02:04:53 +0530

    cors issue fixed

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from config import DB_CONFIG
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=[
+    "https://student-task-manager-mr58.onrender.com"
+])
 
 def get_db():
     return mysql.connector.connect(**DB_CONFIG)
