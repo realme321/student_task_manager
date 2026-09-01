f5f760aa83726f6c3646648442868ea5abad7fcf
Pratik Punj <pratikpunj3@gmail.com>
2026-09-01 22:28:37 +0530

    Connect Flask app to Aiven MySQL

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
-DB_CONFIG ={
-    'host':'yamanote.proxy.rlwy.net',
-    'user':'root',
-    'password':'pCTktxQLRyfpjCGhNWdLZusjLMGpGFOi',
-    'database':'railway',
-    'port':27600
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DB_CONFIG = {
+    'host': os.environ.get('DB_HOST'),
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'database': os.environ.get('DB_NAME'),
+    'port': int(os.environ.get('DB_PORT', 3306)),
+    'ssl_ca': os.path.join(BASE_DIR, 'ca.pem'),
+    'ssl_verify_cert': True
 }
